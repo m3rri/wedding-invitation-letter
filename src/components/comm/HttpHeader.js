83154e2e01011409526878402cb76a2cf765df4a
m3rri <m3rri17@gmail.com>
2022-06-27 23:00:45 +0900

    img path 수정

--- src/components/comm/HttpHeader.js
+++ src/components/comm/HttpHeader.js
@@ -1,13 +1,14 @@
 import Head from 'next/head'
+const {IMAGE_PATH} = process.env;
 
 const HttpHeader = ()=>{
     return <Head>
         <title>🎉양희영❤김혜리 모바일 청첩장🎈</title>
         <meta charSet="utf-8"></meta>
-        <meta property='og:image' content="public/img/thumb.jpg" />
+        <meta property='og:image' content={`./${IMAGE_PATH}/thumb.jpg`} />
         <meta property="og:title" content="🎉양희영❤김혜리 청첩장🎈" />
         <meta property="og:description" content="10월 01일 토요일 12시 30분 저희 결혼합니다!" />
-        <meta property="og:url" content="http://172.30.1.32:3000/" />
+        <meta property="og:url" content="https://m3rri.github.io/wedding-invitation-letter" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="wedding invitation letter for mobile"/>
         <meta property="og:locale" content="en_US"></meta>
