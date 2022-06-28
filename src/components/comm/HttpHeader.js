61f35b9340f59015e825cb1fbf6f662393a6f89b
m3rri <m3rri17@gmail.com>
2022-06-29 02:50:43 +0900

    map 화면 추가

--- src/components/comm/HttpHeader.js
+++ src/components/comm/HttpHeader.js
@@ -1,13 +1,14 @@
-import Head from 'next/head'
+import Head from 'next/head';
 const {IMAGE_PATH} = process.env;
 
 const HttpHeader = ()=>{
+    console.log(process.env);
     return <Head>
         <title>🎉양희영❤김혜리 모바일 청첩장🎊</title>
         <meta charSet="utf-8"></meta>
         <meta property='og:image' content={`./${IMAGE_PATH}/thumb.jpg`} />
         <meta property="og:title" content="🎉양희영❤김혜리 청첩장🎊" />
-        <meta property="og:description" content="10월 01일 토요일 12시 30분 저희 결혼합니다!" />
+        <meta property="og:description" content="10월1일 토요일 12시 30분 저희 결혼합니다!" />
         <meta property="og:url" content="https://m3rri.github.io/wedding-invitation-letter" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="wedding invitation letter for mobile"/>
@@ -15,6 +16,7 @@ const HttpHeader = ()=>{
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
         <link rel="icon" href="/favicon.ico" />
+        <script type="text/javascript" src='//dapi.kakao.com/v2/maps/sdk.js?appkey=7a0f2855bac0bd9122ee680095cf4fac'></script>
     </Head>;
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 import Layout from 'components/comm/Layout';
 import MainPicture from 'components/index/MainPicture';
 import Introduce from 'components/index/Introduce';
+const {IMAGE_PATH} = process.env;
 
 const ArticleWithHeight = styled.article(
   {
@@ -41,19 +42,44 @@ const INTRO_TEXT = [
   더없는 기쁨이 되겠습니다.`
 ];
 
+const createKakaoMap = (ref)=>{
+  const marker = new kakao.maps.LatLng(37.536038, 127.095678);
+  const imgSrc = `/img/mapMarker.png`,
+        imgSize = new kakao.maps.Size(38, 40),
+        imgOption = {offset: new kakao.maps.Point(19, 40)};
+  const markerImg = new kakao.maps.MarkerImage(imgSrc, imgSize, imgOption);
+  const kakaoMap = new kakao.maps.Map(ref.current, {
+    center: marker,
+    level: 3
+  });
+
+  kakaoMap.setDraggable(false);
+  kakaoMap.setZoomable(false);
+
+  new kakao.maps.Marker({
+    position: marker,
+    image: markerImg,
+    clickable: true
+  })
+  .setMap(kakaoMap);
+}
+
 export default function Home() {
   const [pageHeight, setPageHeight] = useState(700);
   const [top, setTop] = useState(100);
 
   const mainPicture = useRef();
   const introduceText = useRef();
-  const map = useRef();
+  const mapWapper = useRef();
+  const mapArea = useRef();
   const rollingPaper = useRef();
 
   useEffect(()=>{
     const {innerHeight} = window;
     setPageHeight(innerHeight);
     setTop(innerHeight/8.1);
+
+    createKakaoMap(mapArea);
   }, []);
 
   return <Layout>
@@ -85,11 +111,61 @@ export default function Home() {
     </ArticleWithHeight>
     <ArticleWithHeight
       css={{
-        height: pageHeight, background: "rgba(196,125,120,1)", color: "#efefef"
+        background: "rgba(196,125,120,0.15)", color: "#8BA5B4"
       }}
-      ref={map}
+      ref={mapWapper}
     >
-      MAP
+      <div style={{margin:"40px 0 10px"}}>오시는 길</div>
+      <p>웨딩스퀘어 강변점 3층 베니르홀</p>
+    </ArticleWithHeight>
+    <ArticleWithHeight
+      css={{
+        background: "rgba(196,125,120,0.15)", color: "#8BA5B4"
+      }}
+      ref={mapWapper}
+    >
+      <div id="map" style={{width:"100%",height:"250px"}} ref={mapArea}></div>
+    </ArticleWithHeight>
+    <ArticleWithHeight
+      css={{
+        background: "rgba(196,125,120,0.15)", color: "#8BA5B4"
+      }}
+      ref={mapWapper}
+    >
+      <div style={{margin:"30px 30px"}}>
+        <p>
+          <a
+            href="https://map.kakao.com/link/search/웨딩스퀘어%20강변점"
+            target={'_blank'}
+            style={{padding:"0 15px"}}>
+            <img src="img/kakaomap.png" width={35} height={30}/>
+            <span style={{padding:"0 0 0 10px"}}>카카오맵</span>
+          </a>
+          <a
+            href="https://m.map.naver.com/search2/search.naver?query=%EC%9B%A8%EB%94%A9%EC%8A%A4%ED%80%98%EC%96%B4%20%EA%B0%95%EB%B3%80%EC%A0%90&sm=shistory&style=v5#/map/1/31761747"
+            target={'_blank'}
+            style={{padding:"0 15px"}}>
+            <span style={{padding:"0 0 0 10px"}}>네이버맵</span>
+          </a>
+        </p>
+        <p>
+          [자차]<br/>
+          네비게이션 | 강변테크노마트 검색<br/>
+          도로명 | 서울시 광진구 광나루로 56길 85<br/>
+          [주차] 강변 테크노마트 지하 3층, 지하 4층<br/>
+          100번대 이용하면 예식장과 가깝습니다.<br/>
+        </p>
+        <p>
+          [지하철] 2호선 강변역<br/>
+          1, 2번 출구사이 연결 통로를 이용
+        </p>
+        <p>
+          [버스]<br/>
+          시외, 고속버스 | 동서울터미널 하차 후 강변역 통로 이용<br/>
+          지선버스 | 2223, 3212, 3214<br/>
+          광역버스 | 1112, 1117, 1650, 1660, 5600, 5700A, 5700B, 9304<br/>
+        </p>
+      </div>
     </ArticleWithHeight>
     <ArticleWithHeight
       css={{
